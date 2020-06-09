05b2d573ca45aeebcae7a8fa9921180d136fc27d
Martin Saulis <martin@saulis.com>
2020-06-10 01:34:04 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './style.css'
+
+function App() {
+  return (
+    <div>
+      initial
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root'))
